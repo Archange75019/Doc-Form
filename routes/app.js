8f5794712308faabab6bf653fcc725138f3d8c89
Archange75019 <arnaud.escalier@gmail.com>
2021-05-04 22:15:15 +0200

    delete event emmitter

--- routes/app.js
+++ routes/app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var EventEmitter = require('events');
+
 
 var user = require('../controllers/user');
 var doc = require('../controllers/docs');
